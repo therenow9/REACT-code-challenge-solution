896c8f4876ac556c68f69f54dd0ba476d584fa8a
Jeremy Scheuerman <60898774+therenow9@users.noreply.github.com>
2022-06-27 20:24:39 -0400

    Initial commit

---
+++ app-frontend/components/Challenge.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Challenge = () => {
+	return (
+		<div className='flex flex-wrap items-center justify-around min-w-full mt-6 sm:w-full'>
+			<div className='p-6 mt-6 text-left border w-full rounded-xl'></div>
+		</div>
+	);
+};
+
+export default Challenge;
